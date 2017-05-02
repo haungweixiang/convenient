75afc5953689d63e6eb616771b2c47144e1d2269
System Administrator <k90218104@gcloud.csu.edu.tw>
2017-05-02 22:58:28 +0800

    add connect.php in 20170502

--- bc/mysql/connect.php
+++ bc/mysql/connect.php
@@ -1,15 +1,35 @@
 <?php
-  try {
-    $username = 'root';
-    $password = '12345678';
-    $dbname="test";
-    $dsn = "mysql:host=localhost;dbname=$dbname";
-    $options = array(
-      PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
-    );
-       $db = new PDO($dsn, $username, $password, $options);
-  } catch (Exception $e) {
+try {
+  $username = 'root';
+  $password = '12345678';
+  $dbname = "convenient";
+  $dsn = "mysql:host=localhost;dbname=$dbname";
+  $options = array(
+    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+  );
+     $db = new PDO($dsn, $username, $password, $options);
+} catch (Exception $e) {
+  echo "連接資料庫失敗";
+}
 
+  class Sql
+  {
+    function select($db, $table_name){//$db 連接資料庫; $table_name 資料表名稱
+      switch ($table_name) {
+        case 'members':
+          
+          break;
+
+        default:
+          # code...
+          break;
+      }
+      function Sql_Select(){
+        $Select_Table = "SELECT * FROM $table_name";
+        $query = $db->query($Select_Table);
+        return $query;
+      }
+    }
   }
 
 
