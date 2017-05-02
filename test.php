e518db8184d88fd65a9b0d129011230c9cb9e62d
System Administrator <k90218104@gcloud.csu.edu.tw>
2017-05-02 23:02:03 +0800

    add test.php test in 20170502

---
+++ test.php
@@ -0,0 +1,14 @@
+<?php
+  include 'bc/mysql/connect.php';
+  $Sql = new Sql();
+  // $que = $Sql->select($db);
+  // $shoe = $que->fetchAll();
+
+
+  $arr = array('Hello','World!','Beautiful','Day!');
+  $count = count($arr);
+  for ($i=1; $i <= $count ; $i++) {
+    
+  }
+
+ ?>
