cde7c0f1da8101f6b58ca5bbb48083ae054aceaf
unknown <k90218104@gcloud.csu.edu.tw>
2017-05-03 16:08:09 +0800

    add store SConvenient table column

--- bc/mysql/connect.php
+++ bc/mysql/connect.php
@@ -14,9 +14,9 @@ try {
 
   class Sql
   {
-    function select($db, $table_name){//$db 連接資料庫; $table_name 資料表名稱
-      switch ($table_name) {
-        case 'members':
+    function select($db, $table_name, $number){//$db 連接資料庫; $table_name 資料表名稱; $number 看是要取得全部欄位資料還是,預設的幾筆
+      switch ($number) {//number=all 全部欄位 * ; number = 1 特定預設欄位
+        case 'all':
           
           break;
 
@@ -24,12 +24,26 @@ try {
           # code...
           break;
       }
-      function Sql_Select(){
-        $Select_Table = "SELECT * FROM $table_name";
-        $query = $db->query($Select_Table);
-        return $query;
-      }
+      switch ($table_name) {
+        case 'members':
+
+          $query = $this->Sql_Select($db, $table_name);
+          $Show = $query->fetchAll();
+          print_r($Show);
+          break;
+
+        default:
+          # code...
+          break;
+      }//switch
+
     }
+    function Sql_Select($db, $table_name){//執行查詢把結果回傳
+      $Select_Table = "SELECT * FROM $table_name";
+      $query = $db->query($Select_Table);
+      return $query;
+    }
+
   }
 
 
