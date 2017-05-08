c6c126c75c9dc9ff52dbca0d0159eba138a8283e
System Administrator <k90218104@gcloud.csu.edu.tw>
2017-05-08 09:10:39 +0800

    up in 20170508

--- test.php
+++ test.php
@@ -2,25 +2,31 @@
   include 'bc/mysql/connect.php';
 
   // $D = $Display->fetchAll();
-  $Sql_Query_Select = function($db, $table_name, $choose){//$db 資料庫連線;$table_name 資料表名稱;$choose 選擇要全部欄位(不輸入或1)或者特定(2)
+  $Sql_Query_Select = function($db, $table_name, $choose){
+  //$db 資料庫連線;$table_name 資料表名稱;$choose 選擇要全部欄位(不輸入或1)或者特定(2)
 
     switch ($table_name) {
       case 'members':
-        if ($choose == "" || $choose == '1') {
-          $Select_Table = "SELECT * FROM $table_name";
-          $query = querys($db, $Select_Columns);
-        }elseif ($choose == '2') {
-          $Select_Columns = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = '".$table_name."'";
-          $query = querys($db, $Select_Columns);
-        }
-        break;
+      if ($choose == "" || $choose == '1') {
+
+        $Select_Table = "SELECT * FROM $table_name";
+        $Display = querys($db, $Select_Columns);
+
+      }elseif ($choose == '2') {
+
+        $Select_Columns = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = '".$table_name."'";
+        $query = querys($db, $Select_Columns);
+        $Display = $query->fetchAll();
+
+      }
+      break;
 
       default:
-        # code...
-        break;
-    }
-    return $query;//執行查詢把結果回傳
-  };//$Sql_Query_Select的結束
+      # code...
+      break;
+    }//switch 結束
+    return $Display;//執行查詢把結果回傳
+  };//$Sql_Query_Select 結束
 
   function querys($db, $Sql_Select){
     $query = $db->query($Sql_Select);
@@ -29,13 +35,16 @@
 
   $Display = $Sql_Query_Select($db, "members", '2');//查詢特定欄位
 
-  $Displays = $Display->fetchAll();
+  echo $Display[2][0];
+  // echo "<pre>";
+  // print_r($Displays = $Display->fetchAll());
+  // echo "</pre>";
 
-    foreach ($Displays as $key => $value) {
-     $Show[$key] = $value[0];//把特定欄位用以索引
-    }
-
-echo $Show[2];//
+//     foreach ($Displays as $key => $value) {
+//      $Show[$key] = $value[0];//把特定欄位用以索引
+//     }
+//
+// echo $Show[2];
   // while($Display = $Sql_Query_Select($db, "members", '2')->fetch(PDO::FETCH_ASSOC)){
   //   echo $Display[0];
   // }
