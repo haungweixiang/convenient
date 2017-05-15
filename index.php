9f34cf39a0bec2fdb126b2d41e9c4482adb17f38
System Administrator <k90218104@gcloud.csu.edu.tw>
2017-05-15 19:58:59 +0800

    up in 20170515

--- index.php
+++ index.php
@@ -31,7 +31,10 @@
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
     <!-- Latest compiled and minified JavaScript -->
-    
+<?php
+  @session_start();
+  $_SESSION["db"] = mysqli_connect();
+ ?>
 </head>
 
 <body>
