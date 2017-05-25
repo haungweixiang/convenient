b82e2f900963c62924cbbb03cfdade2a574ffd53
System Administrator <k90218104@gcloud.csu.edu.tw>
2017-05-25 22:50:50 +0800

    up in 20170525

--- center/SqlCenterApi.php
+++ center/SqlCenterApi.php
@@ -38,27 +38,30 @@ $Balance = function($db){
 
 };//以會員ID為條件，搜尋會員所剩餘額
 
-if ($_SERVER["PHP_SELF"] == "/convenient/center/SqlCenterApi.php") {//判斷網址，此API是被引入還是直接連入
-
+if ($_SERVER['REQUEST_URI'] == "/convenient/center/SqlCenterApi.php") {//判斷網址，此API是被引入還是直接連入
+  // echo '$_SERVER[PHP_SELF]='.$_SERVER["PHP_SELF"]."<br/>";
+  // echo '$_SERVER[REUQEST_URI]='.$_SERVER['REQUEST_URI']."<br/>";
   include '../bcs/mysql/connect.php';
   $SelectDeposit = $Deposit($db);//存款金額
   $SelectBC = $Before_Convenient($db);//查詢曾經訂購過的便當
   $SelectBalance = $Balance($db);//查尋餘額
 
   echo "存款：<br/>";
-  print_r($Deposit_Json = json_encode($SelectDeposit));//存款Json，並顯示
+  echo $Deposit_Json = json_encode($SelectDeposit);//存款Json，並顯示
   echo "<br/>曾經訂購便當<br/>";
-  print_r($BC_Json = json_encode($SelectBC));//曾經訂購的便當Json，並顯示
+  echo($BC_Json = json_encode($SelectBC));//曾經訂購的便當Json，並顯示
   echo "<br/>餘額<br/>";
-  print_r($Balance_Json = json_encode($SelectBalance));//餘額的Json，並顯示
+  echo($Balance_Json = json_encode($SelectBalance));//餘額的Json，並顯示
 
 }else {
+  // echo '$_SERVER[PHP_SELF]='.$_SERVER["PHP_SELF"]."<br/>";
+  // echo '$_SERVER[REUQEST_URI]='.$_SERVER['REQUEST_URI']."<br/>";
 
   include 'bcs/mysql/connect.php';
   $SelectDeposit = $Deposit($db);//存款金額
   $SelectBC = $Before_Convenient($db);//查詢曾經訂購過的便當
   $SelectBalance = $Balance($db);//查尋餘額
-  
+
   $Deposit_Json = json_encode($SelectDeposit);//存款Json
   $BC_Json = json_encode($SelectBC);//曾經訂購的便當Json
   $Balance_Json = json_encode($SelectBalance);//餘額的Json
